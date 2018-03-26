78c0f65f4e2404d989a58d8ca9caf7f828672bb5
Hemesh-Unka <hemeshunka@gmail.com>
2018-03-26 18:14:51 +0100

    fixed issue with postgress

--- index.js
+++ index.js
@@ -6,23 +6,6 @@ var app = express();
 // app.set(‘views’, __dirname + ‘/views’);
 // app.set(‘view engine’, ‘ejs’);
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-// client.connect();
-//
-// client.query('SELECT * FROM test_table;', (err, res) => {
-//   if (err) throw err;
-//   for (let row of res.rows) {
-//     console.log(JSON.stringify(row));
-//   }
-//   client.end();
-// });
-
-
-
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
@@ -30,7 +13,15 @@ app.get('/', function(request, response) {
 });
 
 app.get('/db', function(request, response) {
-  client.query('SELECT * FROM test_table;', (err, res) => {
+
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
+
+  client.connect();
+
+  client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
     if (err) throw err;
     for (let row of res.rows) {
       console.log(JSON.stringify(row));
@@ -38,19 +29,7 @@ app.get('/db', function(request, response) {
     client.end();
   });
 
-  // pool.connect(process.env.DATABASE_URL, function(err, client, done) {
-  //   client.query('SELECT * FROM test_table', function(err, result) {
-  //     done();
-  //     if (err) {
-  //       console.error(err);
-  //       response.send("Error" + err);
-  //     } else {
-  //       console.log(result.rows);
-  //       response.send(result.rows);
-  //     };
-  //   });
-  // });
-  // //pool.end();
+
 });
 
 app.listen(app.get('port'), function() {
