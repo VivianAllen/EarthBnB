efdce99a72827facb0090a0c81ba39ae33439f9a
Hemesh-Unka <hemeshunka@gmail.com>
2018-03-26 18:23:13 +0100

    fixed issue with postgress

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var app = express();
 // app.set(‘views’, __dirname + ‘/views’);
 // app.set(‘view engine’, ‘ejs’);
 
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
@@ -14,23 +19,16 @@ app.get('/', function(request, response) {
 
 app.get('/db', function(request, response) {
 
-  const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-  });
-
   client.connect();
 
   client.query('SELECT * FROM test_table;', (err, res) => {
     if (err) throw err;
     for (let row of res.rows) {
       console.log(JSON.stringify(row));
-      response.send('Hey you got some data from the database!');
+      response.send(row);
     }
     client.end();
   });
-
-
 });
 
 app.listen(app.get('port'), function() {
