1e2deef6f4071dd20fc32045fc60317d6b2f795c
Hemesh-Unka <hemeshunka@gmail.com>
2018-03-26 17:39:55 +0100

    Fixed postgress issue

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var pg = require('pg');
 var app = express();
-var pool = new pg.Pool()
+var pool = new pg.Pool();
 // app.use(express.static(__dirname + ‘/public’));
 // // views is directory for all template files
 // app.set(‘views’, __dirname + ‘/views’);
 // app.set(‘view engine’, ‘ejs’);
-
-// create a pool
-var pool = new pg.Pool()
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -29,7 +26,7 @@ app.get('/db', function(request, response) {
       };
     });
   });
-  pool.end();
+  //pool.end();
 });
 
 app.listen(app.get('port'), function() {
