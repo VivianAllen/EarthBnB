d7c8df678157e496aa2c40c437405700807d4f73
Hemesh-Unka <hemeshunka@gmail.com>
2018-03-26 17:47:33 +0100

    Really fixed postgress issue

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 var express = require('express');
-var pg = require('pg');
+const { Client } = require('pg');
 var app = express();
-var pool = new pg.Pool();
 // app.use(express.static(__dirname + ‘/public’));
 // // views is directory for all template files
 // app.set(‘views’, __dirname + ‘/views’);
 // app.set(‘view engine’, ‘ejs’);
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+// client.connect();
+//
+// client.query('SELECT * FROM test_table;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
+
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -14,19 +30,27 @@ app.get('/', function(request, response) {
 });
 
 app.get('/db', function(request, response) {
-  pool.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err) {
-        console.error(err);
-        response.send("Error" + err);
-      } else {
-        console.log(result.rows);
-        response.send(result.rows);
-      };
-    });
+  client.query('SELECT * FROM test_table;', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
   });
-  //pool.end();
+
+  // pool.connect(process.env.DATABASE_URL, function(err, client, done) {
+  //   client.query('SELECT * FROM test_table', function(err, result) {
+  //     done();
+  //     if (err) {
+  //       console.error(err);
+  //       response.send("Error" + err);
+  //     } else {
+  //       console.log(result.rows);
+  //       response.send(result.rows);
+  //     };
+  //   });
+  // });
+  // //pool.end();
 });
 
 app.listen(app.get('port'), function() {
