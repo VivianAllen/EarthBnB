9857062d106957bd120e5c099652e7303dfac944
Vivian Allen <mrvivianallen@gmail.com>
2018-03-29 13:49:25 +0100

    db admin routes cleaned up, interface added

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 var express = require('express');
 var ejs = require('ejs');
-var bodyParser = require('body-parser');
-var session = require('express-session');
 const db = require('./db');
 
 var app = express();
 // body parser setup
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
-app.use(express.json());       // to support JSON-encoded bodies
-app.use(express.urlencoded()); // to support URL-encoded bodies
-// end body parser setup
-
-app.use(session({secret: 'Ssh'}));
 app.use(express.static(__dirname + '/public'));
 
 app.set('port', (process.env.PORT || 5000));
@@ -22,66 +11,7 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views');
 
 app.get('/', function(request, response) {
-  response.redirect('/properties');
-});
-
-app.get('/signup', function(request, response) {
-  response.render('signup');
-});
-
-
-app.post('/session/add_user', function(request, response) {
-  var username = request.body.username;
-  var password = request.body.password;
-  var queryString = `INSERT INTO bnb_users(username, password) VALUES
-  ('${username}', '${password}')`;
-
-    db.query(queryString, function(err, res) {
-      if (err) {
-        console.log(err.stack);
-      } else {
-        console.log(queryString + ' sucessful!')
-      };
-    });
-  request.session.username=username;
-  response.redirect('/properties')
-});
-
-
-app.get('/properties', function(request, response) {
-  var username = request.session.username || 'anonymous';
-  db.query('SELECT * FROM bnb_properties;', function(err, res) {
-    if (err) {
-      console.log(err.stack);
-    } else {
-      response.render('properties', { results: res.rows, username: username });
-    };
-  });
-});
-
-app.get('/properties/add', function(request, response) {
-  response.render('add_property');
-});
-
-app.get('/test', function(request, response) {
-  response.send('hello world');
-});
-
-app.post('/properties/add', function(request, response) {
-  var imgsrc = request.body.imgsrc;
-  var description = request.body.description;
-  var queryString = `INSERT INTO bnb_properties(imgsrc, description) VALUES
-  ('${imgsrc}', '${description}')`;
-
-    db.query(queryString, function(err, res) {
-      if (err) {
-        console.log(err.stack);
-      } else {
-        console.log(queryString + ' sucessful!')
-      };
-    });
-
-  response.redirect('/')
+  response.render('db_admin');
 });
 
 app.get('/dbs/purge_tables', function(request, response) {
