c40f51930f5d04f28d9bb762af0adb4022e713a3
Chiaki Mizuta <chiakimizuta61@gmail.com>
2018-03-29 12:04:15 +0100

    added sessions

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var ejs = require('ejs');
-var bodyParser = require('body-parser')
-const db = require('./db')
+var bodyParser = require('body-parser');
+var session = require('express-session');
+const db = require('./db');
 
 var app = express();
 // body parser setup
@@ -13,6 +14,7 @@ app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 // end body parser setup
 
+app.use(session({secret: 'Ssh'}));
 app.use(express.static(__dirname + '/public'));
 
 app.set('port', (process.env.PORT || 5000));
@@ -27,7 +29,8 @@ app.get('/signup', function(request, response) {
   response.render('signup');
 });
 
-app.post('/properties', function(request, response) {
+
+app.post('/session/add_user', function(request, response) {
   var username = request.body.username;
   var password = request.body.password;
   var queryString = `INSERT INTO bnb_users(username, password) VALUES
@@ -40,17 +43,18 @@ app.post('/properties', function(request, response) {
         console.log(queryString + ' sucessful!')
       };
     });
-
+  request.session.username=username;
   response.redirect('/properties')
 });
 
 
 app.get('/properties', function(request, response) {
+  var username = request.session.username || 'anonymous';
   db.query('SELECT * FROM bnb_properties;', function(err, res) {
     if (err) {
       console.log(err.stack);
     } else {
-      response.render('properties', { results: res.rows });
+      response.render('properties', { results: res.rows, username: username });
     };
   });
 });
